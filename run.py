e0f0f4165ae893709bf73888786000cbcc065ad7
Julien Lamy <lamy@unistra.fr>
2018-06-12 09:18:18 +0200

    Add license

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+#########################################################################
+# Dicomifier.ws - Copyright (C) Universite de Strasbourg
+# Distributed under the terms of the MIT license. Refer to the 
+# LICENSE.txt file or to https://opensource.org/licenses/MIT for details.
+#########################################################################
+
 import werkzeug
 import dicomifier_ws
 
